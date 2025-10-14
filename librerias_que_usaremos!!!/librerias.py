4c5adeb267fbb807cf5600d0f85bd9f9a7f49e1d
david <davidmanquilef777@gmail.com>
2025-10-13 21:14:17 -0300

    primer cambio !!!

---
+++ librerias_que_usaremos!!!/librerias.py
@@ -0,0 +1,6 @@
+# Librerías necesarias para el proyecto de ajedrez:
+# 1. pygame: Para crear la interfaz gráfica, dibujar el tablero y las piezas.
+# 2. python-chess: Para manejar toda la lógica del juego, como movimientos legales, jaques y el estado del tablero.
+
+#   pip install pygame
+#   pip install python-chess
